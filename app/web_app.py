4d916944e1cd283ae7e9a42272f982db6a7b5146
Mathieu SAVOURE <157049199+MatsaVV@users.noreply.github.com>
2024-04-28 17:26:02 +0200

    upgrade
    apport de la web_app_1.0 + adaptation code + ajout gitignore supplémentaire

---
+++ app/web_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load('model/pipe.joblib')
+
+st.title('Prédiction de prix immobilier')
+Unnamed_0 = st.number_input('Unnamed: 0')
+longitude = st.number_input('Longitude')
+latitude = st.number_input('Latitude')
+housing_median_age = st.number_input('Âge médian des logements')
+total_rooms = st.number_input('Nombre total de pièces')
+total_bedrooms = st.number_input('Nombre total de chambres')
+population = st.number_input('Population')
+households = st.number_input('Ménages')
+median_income = st.number_input('Revenu médian')
+ocean_proximity = st.selectbox('Proximité de l\'océan', ('NEAR BAY', 'INLAND', '<1H OCEAN', 'NEAR OCEAN', 'ISLAND'))
+
+if st.button('Prédire la Valeur de la Maison'):
+    input_data = pd.DataFrame([[Unnamed_0, longitude, latitude, housing_median_age, total_rooms, total_bedrooms, population, households, median_income, ocean_proximity]],
+                                  columns=['Unnamed: 0', 'longitude', 'latitude', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'median_income', 'ocean_proximity'])
+
+    prediction = model.predict(input_data)
+    st.success(f'La valeur médiane prédite de la maison est: ${prediction[0]}')
+
